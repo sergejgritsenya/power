7ddb7a0408fec857d67c1a4dcc608d861388e99e
Sergej <sergejgritsenya@gmail.com>
2020-03-06 17:15:09 +0300

    delete router

--- src/admin/layout/admin-app.tsx
+++ src/admin/layout/admin-app.tsx
@@ -2,7 +2,6 @@ import { createStyles, makeStyles } from "@material-ui/styles"
 import { TRouteComponentProps } from "chyk"
 import { useObserver } from "mobx-react-lite"
 import React, { FC } from "react"
-import { Router, useHistory } from "react-router-dom"
 import { TChykLoadData } from ".."
 import { AdminHeader } from "./header"
 import { AdminMain } from "./main"
@@ -13,18 +12,15 @@ export const adminAppLoader: TChykLoadData<TAdminAppData> = async (_, {}) => ({}
 
 type TAdminAppProps = TRouteComponentProps<TAdminAppData>
 export const AdminApp: FC<TAdminAppProps> = ({ route }) => {
-  const history = useHistory()
   const classes = useStyles()
   return useObserver(() => (
-    <Router history={history}>
-      <div className={classes.root}>
-        <AdminHeader />
-        <div className={classes.flex}>
-          <AdminNavBar />
-          <AdminMain route={route} />
-        </div>
+    <div className={classes.root}>
+      <AdminHeader />
+      <div className={classes.flex}>
+        <AdminNavBar />
+        <AdminMain route={route} />
       </div>
-    </Router>
+    </div>
   ))
 }
 
