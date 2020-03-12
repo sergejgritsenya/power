7981f1de2f704d631c4228f18348e6b96798e593
grey <sergejgritsenya@gmail.com>
2020-03-12 23:20:26 +0300

    main page

--- src/web/components/main/main-page.tsx
+++ src/web/components/main/main-page.tsx
@@ -1,54 +1,36 @@
 import { makeStyles, Typography } from "@material-ui/core"
 import clsx from "clsx"
 import React, { FC } from "react"
-import { HeaderMenuItem } from "../../layout/header/header-item"
-import { WebFooter } from "../../layout/web-footer"
-import { PageInner } from "../common/page-inner"
+import { MainMenu } from "./main-menu"
 
 export const WebMainPage: FC = () => {
   const classes = useStyles()
   return (
-    <>
-      <main className={classes.root}>
-        <div className={classes.content}>
-          <img
-            src="https://static.tildacdn.com/tild3437-6665-4835-b834-366333316638/OLYMPIC13_1.jpg"
-            className={clsx(classes.contentItem, classes.logo)}
-          />
-          <div className={classes.contentItem}>
-            <Typography className={clsx(classes.text, classes.textTitle)}>P.O.W.E.R. </Typography>
-            <Typography className={classes.text}>
-              {"Promotion of Olympic Wrestling \n in European Region"}
-            </Typography>
-          </div>
+    <main className={classes.root}>
+      <div className={classes.content}>
+        <img
+          src="https://static.tildacdn.com/tild3437-6665-4835-b834-366333316638/OLYMPIC13_1.jpg"
+          className={clsx(classes.contentItem, classes.logo)}
+        />
+        <div className={classes.contentItem}>
+          <Typography className={clsx(classes.text, classes.textTitle)}>P.O.W.E.R. </Typography>
+          <Typography className={classes.text}>
+            {"Promotion of Olympic Wrestling \n in European Region"}
+          </Typography>
         </div>
-        <Typography component="div" className={clsx(classes.text, classes.textFooter)}>
-          by Classic Sport LLC
-        </Typography>
-        <PageInner component="nav" className={classes.nav}>
-          <div className={classes.text}>
-            <HeaderMenuItem label="News" link="/news"></HeaderMenuItem>
-          </div>
-          <div className={classes.text}>
-            <HeaderMenuItem label="Tournaments" link="/tournaments"></HeaderMenuItem>
-          </div>
-          <div className={classes.text}>
-            <HeaderMenuItem label="Shop" link="/shop"></HeaderMenuItem>
-          </div>
-        </PageInner>
-      </main>
-      <WebFooter />
-    </>
+      </div>
+      <Typography component="div" className={clsx(classes.text, classes.textFooter)}>
+        by Classic Sport LLC
+      </Typography>
+      <MainMenu />
+    </main>
   )
 }
 
 const useStyles = makeStyles(theme => ({
   root: {
     background: theme.palette.primary.light,
-    minHeight: "calc(100vh - 110px)",
-    [theme.breakpoints.down("sm")]: {
-      minHeight: "calc(100vh - 231px)",
-    },
+    minHeight: "100vh",
   },
   logo: {
     width: "290px",
@@ -68,14 +50,15 @@ const useStyles = makeStyles(theme => ({
     },
   },
   textFooter: {
-    marginTop: "140px",
+    marginTop: "50px",
   },
   content: {
     display: "grid",
     gridTemplateColumns: "1fr 1fr",
-    gridGap: "140px",
+    gridGap: "70px",
     paddingTop: "20vh",
     [theme.breakpoints.down("sm")]: {
+      paddingTop: "0vh",
       gridTemplateColumns: "1fr",
       gridTemplateRows: "1fr",
     },
@@ -90,15 +73,6 @@ const useStyles = makeStyles(theme => ({
       "&:last-child": {
         justifySelf: "center",
       },
-    },
-  },
-  nav: {
-    display: "flex",
-    justifyContent: "center",
-    marginTop: "180px",
-    [theme.breakpoints.down("sm")]: {
-      flexDirection: "column",
-      alignItems: "center",
     },
   },
 }))
